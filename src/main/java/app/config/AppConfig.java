5e490c6a3de4baabc8c6d252b2858e1b2ac66aa8
Furgon17 <pawel.grackov@gmail.com>
2023-10-28 14:17:46 +0300

    Spring Task 2

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -6,7 +6,7 @@ import app.model.Timer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+
 
 @Configuration
 @ComponentScan(basePackages = "app")
